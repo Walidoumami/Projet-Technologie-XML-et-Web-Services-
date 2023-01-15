49434e4e9f4fe6977196e80d2f456abb1cef5944
Walidoumami <122458838+Walidoumami@users.noreply.github.com>
2023-01-15 17:29:41 +0100

    Create AppTest.java

---
+++ Parties/AppTest.java
@@ -0,0 +1,20 @@
+Paquet MA. exemple. devoir_ws_xml;
+
+Importer une organisation statique. junit. Affirmer. assertTrue;
+
+Importer l’organisation. junit. Test;
+
+/**
+ * Test unitaire pour application simple.
+ */
+AppTest de classe  publique 
+{
+    /**
+ * Test rigoureux :-)
+     */
+    @Test
+    public void devraitAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
